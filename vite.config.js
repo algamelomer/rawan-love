f7b80e5ee7438ede186d40280c151f2119ef518b
omar algamel <123903790+algamelomer@users.noreply.github.com>
2024-09-29 03:17:00 +0300

    .

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,40 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa';
 import path from 'path';
 
 export default defineConfig({
     resolve: {
         alias: {
-            '@': path.resolve(__dirname, 'src'), // Ensure this points to your src directory
+            '@': path.resolve(__dirname, 'src'),
         },
     },
-    plugins: [vue()],
+    plugins: [
+        vue(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+            manifest: {
+                name: 'i love rawan',
+                short_name: 'LoveRawan',
+                description: 'An app made to descripe my love to rawan magdy',
+                theme_color: '#ffffff', // Set theme color to white
+                background_color: '#ffffff',
+                display: 'standalone',
+                scope: '/',
+                start_url: '/',
+                icons: [{
+                        src: '/192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                    },
+                    {
+                        src: '/512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    },
+                ],
+            },
+        }),
+    ],
 });
